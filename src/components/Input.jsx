af20350800a5de9e2a9bdb8a33680ff1b6e83e5a
victorgfz <victor.sjb.2002@gmail.com>
2024-01-09 20:02:08 -0300

    formulario finalizado

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Input = ({ type, placeholder, id, name }) => {
+const Input = ({ type, placeholder, id, name, value, setValue }) => {
   return (
     <input
       className={`text-[#111] bg-transparent font-light outline-none p-2 relative border-b-[1px] border-[#111] w-full ${
@@ -10,6 +10,8 @@ const Input = ({ type, placeholder, id, name }) => {
       placeholder={placeholder}
       id={id}
       name={name}
+      value={value}
+      onChange={({ target }) => setValue(target.value)}
     />
   );
 };
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,6 +4,22 @@ import { motion } from "framer-motion";
 import { transition1 } from "../transitions";
 
 const Contact = () => {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+  const [sent, setSent] = React.useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (name || email || message) {
+      setSent(true);
+      setName("");
+      setEmail("");
+      setMessage("");
+      setTimeout(() => setSent(false), 3000);
+    }
+  }
+
   return (
     <section className="bg-[#111] h-screen w-full overflow-hidden flex justify-center items-center">
       <div className="container px-5 flex flex-col justify-center items-center z-10">
@@ -17,19 +33,36 @@ const Contact = () => {
           get in touch
         </motion.h1>
         <motion.form
+          onSubmit={handleSubmit}
           initial={{ opacity: 0, y: "50%" }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: "50%" }}
           transition={transition1}
           className="flex flex-wrap justify-center gap-5"
         >
-          <Input type="text" placeholder="Name" id="name" name="name" />
-          <Input type="email" placeholder="Email" id="email" name="email" />
+          <Input
+            type="text"
+            placeholder="Name"
+            id="name"
+            name="name"
+            value={name}
+            setValue={setName}
+          />
+          <Input
+            type="email"
+            placeholder="Email"
+            id="email"
+            name="email"
+            value={email}
+            setValue={setEmail}
+          />
           <Input
             type="text"
             placeholder="Message"
             id="message"
             name="message"
+            value={message}
+            setValue={setMessage}
           />
           <button
             className="text-[16px] font-light uppercase border-[1px] border-[#111] py-2 px-[36px] hover:text-white
@@ -38,7 +71,19 @@ const Contact = () => {
             Send a message
           </button>
         </motion.form>
+        {sent && (
+          <motion.p
+            initial={{ opacity: 0, y: "-50%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "-50%" }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className="absolute text-[#111] z-20 bottom-[124px] font-medium "
+          >
+            Message sent with success! Thank You.
+          </motion.p>
+        )}
       </div>
+
       <motion.div
         initial={{ y: "50%" }}
         animate={{ y: 0 }}
